986d397e981075d6a56fbd4bd0314da5bf1c139c
Noor Mohammed Priom <142662091+SOrtINgmASteR@users.noreply.github.com>
2025-08-11 13:00:20 +0600

    disk FCFS

---
+++ Disk_FCFS.py
@@ -0,0 +1,9 @@
+# disk FCFS
+request_sequence = [0, 16, 24, 43, 50, 82, 100, 140, 150, 170, 190, 199]
+head = 50
+idx = -1
+for i in range(0, len(request_sequence)):
+    if request_sequence[i] == head:
+        idx = i
+steps = (request_sequence[len(request_sequence) - 1] - request_sequence[0]) + (request_sequence[idx] - request_sequence[0])
+print(steps)
