a5aa949f0ea232c8f9ce296557390661aaf20b3d
Noor Mohammed Priom <noor105464@gmail.com>
2025-08-15 18:50:23 +0600

    Final commit for Lab report - 5

--- 05_Disk_Scheduling_FCFS_week_5.py
+++ 05_Disk_Scheduling_FCFS_week_5.py
@@ -1,4 +1,4 @@
-# disk scheduling - FCFS
+# Disk scheduling - FCFS
 request_sequence = [0, 16, 24, 43, 50, 82, 100, 140, 150, 170, 190, 199]
 head = 50
 idx = -1
@@ -6,4 +6,4 @@ for i in range(0, len(request_sequence)):
     if request_sequence[i] == head:
         idx = i
 steps = (request_sequence[len(request_sequence) - 1] - request_sequence[0]) + (request_sequence[idx] - request_sequence[0])
-print(steps)
+print(f"Number of Steps: {steps}")
