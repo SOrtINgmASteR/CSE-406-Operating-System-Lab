ad806c2cc9a558d4025db41f9c3fd95889f7ee71
Noor Mohammed Priom <142662091+SOrtINgmASteR@users.noreply.github.com>
2025-09-29 13:18:41 +0600

    Page Replacement FIFO

---
+++ 09_Page_Replacement_FIFO_week_11.py
@@ -0,0 +1,25 @@
+# Page Replacemnt - FIFO
+frames = []
+page_sequence = [] 
+page_faults = 0
+page_hit = 0
+
+frame_size = int(input("Enter Frame Size: "))
+n = int(input("Enter number of sequence: "))
+for i in range(n):
+    x = input(f"Enter {i+1} st Sequence: ")
+    page_sequence.append(x)
+
+for page in page_sequence:
+    if page not in frames:
+        page_faults += 1
+        if len(frames) < frame_size:
+            frames.append(page)
+        else:
+            frames.pop(0)
+            frames.append(page)
+    else:
+        page_hit += 1
+
+print(f"Page Hits: {page_hit}")
+print(f"Page Faults: {page_faults}")
